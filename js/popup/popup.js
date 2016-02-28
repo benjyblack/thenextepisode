c9f1e082b1aca1bee1b7cec1dff42057c2a9d072
Ben Black <ben.black@sweetiq.com>
2016-02-28 16:01:37 -0500

    Convert navigation to CycleJS

--- js/popup/popup.js
+++ js/popup/popup.js
@@ -1,74 +1,139 @@
+const Cycle = require('@cycle/core');
+const {button, ul, li, i, span, makeDOMDriver} = require('@cycle/dom');
+const {Observable} = require('rx');
 const $ = require('jquery');
-const _ = require('lodash');
-const { BASE_URL, NO_EPISODES_MSG } = require('../shared/constants');
 
 const AppState = chrome.extension.getBackgroundPage().AppState;
 
-let linksEl,
-  progressEl;
+var drivers = {
+  DOM: makeDOMDriver('body'),
+  AppState(action$) {
+    return action$.startWith('').map(function (action) {
+      if (action.length) {
+        AppState[action]();
+      }
 
-const renderNav = (AppState) => {
-  $('#series-name-text').text(AppState.series.name);
-  $('#season-name-text').text(`Season ${AppState.season.number}`);
-  $('#episode-name-text').text(`Episode ${AppState.episode.number} - ${AppState.episode.name}`);
-};
-
-const renderLinks = (episodeLinks) => {
-  progressEl.hide();
-
-  const episodeListItems = _.map(episodeLinks, (link) => {
-    return `<tr class="link" data-url="${link.url}">
-      <td class='website'>${link.host.split('.')[0]}</td>
-      <td class='views'>${link.views}</td>
-      <td class='rating'>${link.rating}</td>
-    </tr>`
-  });
-
-  if (!episodeListItems.length) {
-    return linksEl.html(NO_EPISODES_MSG);
+      return AppState;
+    });
   }
-
-  linksEl.html(episodeListItems.join(''));
 };
 
-const onClickNextEpisode = () => {
-  var nextEpisode = AppState.getNextEpisode();
-  AppState.getEpisodeLinks(nextEpisode.url).then(function (links) {
-    openUrl(_.first(links).url);
-  });
-};
+console.log(drivers);
 
-const onClickNavButton = ({ currentTarget: el }) => {
-  var action = $(el).attr('data-action');
-  AppState[action]();
-  onStateChanged(AppState);
-};
+// DOM Read: click next series
+// AppState Write: Change episode
+// DOM Write: Current state
 
-const onClickLink = ({ currentTarget: el }) => {
-  openUrl($(el).attr('data-url'));
-};
+function main({ DOM, AppState }) {
+  const click$ = DOM
+    .select('.btn')
+    .events('click');
 
-const onStateChanged = (AppState) => {
-  linksEl.html('');
-  progressEl.show();
-
-  renderNav(AppState);
-  return AppState
-    .getEpisodeLinks(AppState.episode.url)
-    .then(renderLinks);
-};
-
-const openUrl = (url) => {
-  chrome.tabs.create({ url: BASE_URL + url });
-};
+  return {
+    DOM: AppState.map(function (state) {
+      return ul([
+        li('#getNextEpisode.quick-next-episode.btn.waves-effect.waves-light', 'Next Episode'),
+        li('.episode-info-container', [
+          span('#getPreviousSeries.btn.waves-effect.waves-light.nav-button', [
+            i('.small.material-icons.left', 'skip_previous')
+          ]),
+          span('#series-name-text.episode-info', state.series.name),
+          span('#getNextSeries.btn.waves-effect.waves-light.nav-button', [
+            i('.small.material-icons.right', 'skip_next')
+          ])
+        ]),
+        li('.episode-info-container', [
+          span('#getPreviousSeason.btn.waves-effect.waves-light.nav-button', [
+            i('.small.material-icons.left', 'skip_previous')
+          ]),
+          span('#season-name-text.episode-info', `Season ${state.season.number}`),
+          span('#getNextSeason.btn.waves-effect.waves-light.nav-button', [
+            i('.small.material-icons.right', 'skip_next')
+          ])
+        ]),
+        li('.episode-info-container', [
+          span('#getPreviousEpisode.btn.waves-effect.waves-light.nav-button', [
+            i('.small.material-icons.left', 'skip_previous')
+          ]),
+          span('#episode-name-text.episode-info', `Episode ${state.episode.number} - ${state.episode.name}`),
+          span('#getNextEpisode.btn.waves-effect.waves-light.nav-button', [
+            i('.small.material-icons.right', 'skip_next')
+          ])
+        ])
+      ]);
+    }),
+    AppState: click$.map(ev => ev.currentTarget.id)
+  };
+}
 
 $(document).ready(() => {
-  linksEl = $('#links');
-  progressEl = $('#progress-bar');
-
-	onStateChanged(AppState);
-
-  $('#next-episode').click(onClickNextEpisode);
-	$('.nav-button').click(onClickNavButton);
-  $('body').on('click', '.link', onClickLink);
+  Cycle.run(main, drivers);
 });
+
+//const _ = require('lodash');
+//const { BASE_URL, NO_EPISODES_MSG } = require('../shared/constants');
+//
+//
+//let linksEl,
+//  progressEl;
+//
+//
+//const renderLinks = (episodeLinks) => {
+//  progressEl.hide();
+//
+//  const episodeListItems = _.map(episodeLinks, (link) => {
+//    return `<tr class="link" data-url="${link.url}">
+//      <td class='website'>${link.host.split('.')[0]}</td>
+//      <td class='views'>${link.views}</td>
+//      <td class='rating'>${link.rating}</td>
+//    </tr>`
+//  });
+//
+//  if (!episodeListItems.length) {
+//    return linksEl.html(NO_EPISODES_MSG);
+//  }
+//
+//  linksEl.html(episodeListItems.join(''));
+//};
+//
+//const onClickNextEpisode = () => {
+//  var nextEpisode = AppState.getNextEpisode();
+//  AppState.getEpisodeLinks(nextEpisode.url).then(function (links) {
+//    openUrl(_.first(links).url);
+//  });
+//};
+//
+//const onClickNavButton = ({ currentTarget: el }) => {
+//  var action = $(el).attr('data-action');
+//  AppState[action]();
+//  onStateChanged(AppState);
+//};
+//
+//const onClickLink = ({ currentTarget: el }) => {
+//  openUrl($(el).attr('data-url'));
+//};
+//
+//const onStateChanged = (AppState) => {
+//  linksEl.html('');
+//  progressEl.show();
+//
+//  renderNav(AppState);
+//  return AppState
+//    .getEpisodeLinks(AppState.episode.url)
+//    .then(renderLinks);
+//};
+//
+//const openUrl = (url) => {
+//  chrome.tabs.create({ url: BASE_URL + url });
+//};
+//
+//$(document).ready(() => {
+//  linksEl = $('#links');
+//  progressEl = $('#progress-bar');
+//
+//	onStateChanged(AppState);
+//
+//  $('#next-episode').click(onClickNextEpisode);
+//	$('.nav-button').click(onClickNavButton);
+//  $('body').on('click', '.link', onClickLink);
+//});
